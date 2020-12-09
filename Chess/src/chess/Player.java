ce41fdd610d5c95aaaba97f0e397911bad348244
Hello-there-1 <60330592+Hello-there-1@users.noreply.github.com>
2020-12-09 09:59:30 -0800

    Update Player.java

--- Chess/src/chess/Player.java
+++ Chess/src/chess/Player.java
@@ -1,44 +1,31 @@
-
 package chess;
 
-import java.awt.Color;
+import java.awt.*;
 
-public class Player {
+public class Piece
+{
+    private Type type;
+    public static enum Type {PAWN,BISHOP,KNIGHT,ROOK,QUEEN,KING};   
+    private Player player;
     
-    private static Player currentTurn;
-    private static Player players[] = new Player[2];
-    private int index;   
-
-    public static void Reset()
+    Piece(Player _player, Type _type)
     {
-        players[0] = new Player(1);
-        players[1] = new Player(2);
-        currentTurn = players[0];
-    }
-    public static Player GetCurrentPlayer()
-    {
-        return(currentTurn);
-    }
-    public static void SwitchTurn()
-    {
-        if (currentTurn == players[0])
-            currentTurn = players[1];
-        else
-            currentTurn = players[0];
-    }    
-    
-    Player(int _index)
-    {
-        index = _index;
-    }
-    public static Player getPlayer1()
-    {
-        return (players[0]);
-    }
-    public static Player getPlayer2()
-    {
-        return (players[1]);
-    }
-
+        player = _player;
+        type = _type;
         
+    }
+    public Player getPlayer()
+    {
+        return (player);
+    }
+
+    public void draw(Graphics2D g,int row,int column,int xdelta,int ydelta) {
+        g.setColor(Color.black);
+        if(player == Player.getPlayer1())
+            g.drawOval(Window.getX(column*xdelta+(xdelta/8)), Window.getY(row*ydelta+(ydelta/8)), 3*xdelta/4, 3*ydelta/4);
+        else if(player == Player.getPlayer2())
+            g.fillOval(Window.getX(column*xdelta+(xdelta/8)), Window.getY(row*ydelta+(ydelta/8)), 3*xdelta/4, 3*ydelta/4);
+    // Circle pog
+    // yes Aiden, circle pog indeed
+    }
 }
